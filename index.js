306fbe95e675d7120e9209deef3a28f625593e52
Raul Sanchez <raulsg93@gmail.com>
2020-06-23 20:04:52 -0500

    started on solution, have a fetch, now need db,json

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ alertH1.addEventListener('click', ()=>{
     console.log('we have been clicked')
     alert('the H1 was indeed clicked')
 })
+// lets grab the h1 with the id of 'fetchClick'
+let fetchH1 =  document.getElementById('fetchClick')
+//referencing that h1, lets add an eventListener to listen for a 'click'
+fetchH1.addEventListener('click', ()=>{
+    console.log('is this click working?') //always test your work, if the console log is working, we can move on
+    fetch('')
+})
